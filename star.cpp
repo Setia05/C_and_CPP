5e40b9b5ff8174d34b66676ee463a46ad863f86a
Shubham Setia <54857010+Setia05@users.noreply.github.com>
2019-10-27 22:27:49 -0400

    Create star.cpp

---
+++ star.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+    using namespace std;
+    int main(){ 
+    int n=8;
+
+    while(n--){
+        int m = 8;
+        if(n%2!=0){
+            while(m--){
+                cout<<"*";
+            }
+        }
+        else{
+            cout<<"  ";
+            while(m--){
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+    }
